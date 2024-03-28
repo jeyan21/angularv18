fb8c60597a20041ec84af9d9a397f9e23162407c
jeyan raj <jeyan21@gmail.com>
2024-03-29 00:01:39 +0530

    Social login partial commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,18 @@ import { AppComponent } from './app.component';
 import { SpinnerComponent } from './shared/spinner.component';
 import { SignInComponent } from './shared/sign-in/sign-in.component';
 
+import {
+  GoogleLoginProvider,
+  SocialLoginModule,
+  FacebookLoginProvider,
+  AmazonLoginProvider,
+  VKLoginProvider,
+  MicrosoftLoginProvider,
+  SocialAuthServiceConfig,
+  GoogleSigninButtonDirective,
+  GoogleSigninButtonModule
+} from '@abacritt/angularx-social-login';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -40,11 +52,47 @@ import { SignInComponent } from './shared/sign-in/sign-in.component';
     FullComponent,
     NavigationComponent,
     SidebarComponent,
+    SocialLoginModule,
+    GoogleSigninButtonModule
   ],
   providers: [
     {
       provide: LocationStrategy,
       useClass: PathLocationStrategy
+    },
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: true,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider(
+              '384986639795-b3df1qvrg8rh2o1ih92hlbqo4sl6v3v3.apps.googleusercontent.com'
+            ),
+          },
+          {
+            id: FacebookLoginProvider.PROVIDER_ID,
+            provider: new FacebookLoginProvider('561602290896109'),
+          },
+          {
+            id: AmazonLoginProvider.PROVIDER_ID,
+            provider: new AmazonLoginProvider(
+              'amzn1.application-oa2-client.f074ae67c0a146b6902cc0c4a3297935'
+            ),
+          },
+          {
+            id: VKLoginProvider.PROVIDER_ID,
+            provider: new VKLoginProvider('7624815'),
+          },
+          {
+            id: MicrosoftLoginProvider.PROVIDER_ID,
+            provider: new MicrosoftLoginProvider(
+              '0611ccc3-9521-45b6-b432-039852002705'
+            ),
+          },
+        ],
+      } as SocialAuthServiceConfig,
     },
   ],
   bootstrap: [AppComponent]
--- src/app/shared/sign-in/sign-in.component.ts
+++ src/app/shared/sign-in/sign-in.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { GoogleLoginProvider, SocialAuthService } from '@abacritt/angularx-social-login';
+import { SocialUser } from '@abacritt/angularx-social-login';
+import {
+  FacebookLoginProvider,
+  AmazonLoginProvider,
+  VKLoginProvider,
+  MicrosoftLoginProvider,
+} from '@abacritt/angularx-social-login';
+
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.scss']
 })
-export class SignInComponent {
+export class SignInComponent implements OnInit{
+
+  user: SocialUser | undefined;
+  GoogleLoginProvider = GoogleLoginProvider;
+
+  private accessToken = '';
+
+  constructor(private _authService:SocialAuthService,  private httpClient: HttpClient){}
+
+ 
 
   ngOnInit(): void {
     const signUpButton: HTMLElement = document.getElementById('signUp') as HTMLElement;
@@ -19,5 +39,34 @@ export class SignInComponent {
     signInButton.addEventListener('click', () => {
       container.classList.remove("right-panel-active");
     });
+
+    this._authService.authState.subscribe((user) => {
+      this.user = user;
+    });
   }
+
+  signInWithFB(): void {
+    this._authService.signIn(FacebookLoginProvider.PROVIDER_ID);
+  }
+
+  signInWithAmazon(): void {
+    this._authService.signIn(AmazonLoginProvider.PROVIDER_ID);
+  }
+
+  signInWithVK(): void {
+    this._authService.signIn(VKLoginProvider.PROVIDER_ID);
+  }
+
+  signInWithMicrosoft(): void {
+    this._authService.signIn(MicrosoftLoginProvider.PROVIDER_ID);
+  }
+
+  signOut(): void {
+    this._authService.signOut();
+  }
+
+  refreshGoogleToken(): void {
+    this._authService.refreshAuthToken(GoogleLoginProvider.PROVIDER_ID);
+  }
+
 }
